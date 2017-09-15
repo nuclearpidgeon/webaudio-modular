432bd841888b84f757afff71f76de0c94cc7ff00
Stewart Webb <stewart.james.webb@gmail.com>
2017-09-15 18:55:21 +0200

    Add basic oscillator list demo

--- src/components/SynthBoard.jsx
+++ src/components/SynthBoard.jsx
@@ -1,23 +1,59 @@
 import React from 'react'
 
+const startFreq = 400
+
+let globalCounter = 0
+let nextFreq = startFreq
+let nextFreqDivider = 2
+
 export default class SynthBoard extends React.Component {
     constructor(props) {
         super(props)
+        this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
         this.state = {
             modules: []
         }
 
         this.addModule = this.addModule.bind(this)
+        this.removeModule = this.removeModule.bind(this)
     }
 
     addModule() {
+        globalCounter += 1
+        
+        let newOsc = this.audioContext.createOscillator()
+        newOsc.frequency.value = nextFreq
+        nextFreq = nextFreq + (startFreq / (globalCounter * 2))
+        newOsc.start()
+        newOsc.connect(this.audioContext.destination);
+
         this.setState((prevState) => ({
             ...prevState,
             modules: [
                 ...prevState.modules,
-                "AAA"
+                {
+                    name: "AAA" + globalCounter.toString(),
+                    osc: newOsc
+                }
             ]
         }))
+    }
+
+    removeModule(id) {
+        this.state.modules.filter((mod) => (mod.name == id))[0].osc.stop()
+        this.setState((prevState) => ({
+            ...prevState,
+            modules: [
+                ...prevState.modules.filter((mod) => (mod.name != id))
+            ]
+        }))
+
+    }
+
+    componentDidUpdate() {
+        // this.state.modules.forEach((module) => {
+        //     if (module.running && module.osc)
+        // })
     }
 
     render() {
@@ -28,7 +64,11 @@ export default class SynthBoard extends React.Component {
                 <ul>
                     {
                         this.state.modules.map((module) => (
-                            <li>{ module }</li>
+                            <li key={module.name}>
+                                { module.name }
+                                <br />
+                                <button onClick={(e)=>{ this.removeModule(module.name) }}>x</button>
+                            </li>
                         ))
                     }
                 </ul>
